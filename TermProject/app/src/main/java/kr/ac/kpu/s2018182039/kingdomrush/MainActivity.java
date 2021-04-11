271117e2bf2934aa96e38b236d689c1d5549f907
Gyubeom Han <sy0871@naver.com>
2021-04-11 20:12:51 +0900

    Init base of term project

---
+++ TermProject/app/src/main/java/kr/ac/kpu/s2018182039/kingdomrush/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.kpu.s2018182039.kingdomrush;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
